5f761f19a544076053333d7c09772125e29db641
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-06-28 17:30:17 -0500

    KNN Secuential

--- back-end/src/routes/routes.py
+++ back-end/src/routes/routes.py
@@ -4,6 +4,7 @@ from flask import send_file, Response, flash, request, redirect, url_for, send_f
 from werkzeug.utils import secure_filename
 from models.models import Imagen
 from database.database import db
+from knn import *
 
 # Allowed upload file extensions
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'gif'}
@@ -57,7 +58,7 @@ def Routes (app):
         #If the file exist and has an allowed extension
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)       #Check if the file has a secure filename
-            path = '/uploads/' + filename
+            path = '/images/' + filename
 
             # Check if the file is already uploaded
             existing_file_path = Imagen.query.filter(Imagen.path == path).first()
@@ -77,12 +78,20 @@ def Routes (app):
             return Response(json.dumps({'path': file_path, 'filename': filename, 'id': new_img.id}), status = 200, mimetype = 'application/json')
 
     """
-    API: Get a file by filename from the directory
+    API: Get a uploaded file by filename from the directory
     """
     @app.route('/uploads/<filename>', methods = ['GET'])
     def uploaded_file(filename):
         return send_from_directory(
             app.config['UPLOAD_FOLDER'],filename)
+
+    """
+    API: Get a stored file by filename from the directory
+    """
+    @app.route('/storage/<folder>/<filename>', methods = ['GET'])
+    def stored_file(folder, filename):
+        return send_from_directory(
+            app.config['STORAGE_FOLDER'] + '/' + folder, filename)
     
     """
     API: Get all the uploaded files
@@ -90,6 +99,61 @@ def Routes (app):
     @app.route('/uploads', methods = ['GET'])
     def get_all_uploaded_files():
         return jsonify({'images': list(map(lambda img: img.serialize(), Imagen.query.all()))})
+
+    """
+    API: KNN Secuential - Euclidian Distance
+    """
+    @app.route('/secuential', methods=['POST'])
+    def KNN_Secuential():
+
+        if 'file' not in request.files:
+            flash('No file part')
+        file = request.files['file']
+        
+        if 'k' not in request.form.keys():
+            flash('Pending arguments')
+        k = int(request.form['k'])
+
+        if 'function' not in request.form.keys():
+            flash('Pending arguments')
+        distance_function = int(request.form['function'])
+
+        if file.filename == '':
+            flash('No selected file')
+        
+        #If the file exist and has an allowed extension
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)       #Check if the file has a secure filename
+            path = '/images/' + filename
+
+            # Check if the file is already uploaded
+            existing_file_path = Imagen.query.filter(Imagen.path == path).first()
+            if existing_file_path:
+                return Response(json.dumps({'message': 'File already uploaded'}), status = 404, mimetype = 'application/json')            
+
+            # If not already upload, save the file in the server    
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            # Save the filename and path in a database
+            file_path = url_for('uploaded_file', filename=filename)
+            new_img = Imagen(path = file_path, filename = filename)
+            db.session.add(new_img)
+            db.session.commit()
+            
+            neighbors = []
+            knn = []
+
+            if distance_function == 'euclidian':
+                knn = KNN_Secuential(path, k, euclidian_distance)
+            
+            if distance_function == 'manhattan':
+                knn = KNN_Secuential(path, k, manhattan_distance)
+            
+            for d, neighbour in knn:
+                neighbors.append('storage/' + neighbour)
+            
+            #Return the path and status of the uploaded file
+            return Response(json.dumps({'path': file_path, 'filename': filename, 'id': new_img.id, 'neighbors': neighbors}), status = 200, mimetype = 'application/json')
     
     
     
