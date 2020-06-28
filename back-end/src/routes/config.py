9d76595aa8e1f7d06a3e690329a4c7539bdb805a
rjhans7 <roosevelt.ubaldo@outlook.com>
2020-06-28 14:56:41 -0500

    fixes path

--- back-end/src/routes/config.py
+++ back-end/src/routes/config.py
@@ -5,6 +5,6 @@ Set the configurations of the flask server
 """
 def Config (app):
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-    app.config['UPLOAD_FOLDER'] = "src/images"
+    app.config['UPLOAD_FOLDER'] = "images"
     app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
     cors = CORS(app, resources={r"/*": {"origins": "*"}})
